63d8ac94c607fc467d7c1c4cbac2dc2739f0baa7
Harshit Agrawalgit <harshit.agrawal.9955@gmail.com>
2022-02-01 01:32:59 +0530

    test

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ from constants.NseConstants import NseConstants
 from repository.OptionOiMongoRepository import OptionOiMongoRepository
 from constants.RepositoryConstants import RepositoryConstants
 from service.OptionOiConvertRawDataService import OptionOiConvertRawDataService
-import json
 
 
-# optionDataService = OptionOiConvertRawDataService()
-# data = None
-# count = 0
-# while data == None and count != NseConstants.MAX_RETRY.value:
-#     data = optionDataService.getOptionExpiryData("NIFTY")
+optionDataService = OptionOiConvertRawDataService()
+data = None
+count = 0
+while data == None and count != NseConstants.MAX_RETRY.value:
+    data = optionDataService.getOptionExpiryData("NIFTY")
 
-# repo = OptionOiMongoRepository(RepositoryConstants.MONGO_URL.value, RepositoryConstants.DATABASE.value)
+repo = OptionOiMongoRepository(RepositoryConstants.MONGO_URL.value, RepositoryConstants.DATABASE.value)
 
 
 # print(repo.getRecord("NIFTY", "03-Feb-2022"))
 
 # repo.updateStrikePriceData("NIFTY", data)
 
-# print(count)
+print(count)
 
